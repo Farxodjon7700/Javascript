1d57ff417a75a654b9cb04552f02ea612f5f40f9
Star <Star@gmail.com>
2022-04-06 05:21:01 +0500

    git hub ishim

---
+++ scirpt.js
@@ -0,0 +1,23 @@
+
+let ism = prompt('ismingizni kiriting');
+let yosh = prompt('yoshingizni kiriting');
+let misol = prompt('misolni yechin 17+9');
+let misol1 = prompt('misolni yechin 56*9')
+let misol2 = prompt('misolni yechin 6*10')
+let misol3 = prompt('misolni yechin 25*8');
+let misol4 = prompt('misolni yechin 78*2');
+let misol5 = prompt('misolni yechin 19*9');
+let misol6 = prompt('misolni yechin 25*7');
+let misol7 = prompt('misolni yechin 39*4');
+alert ("javobni ko'rish uchun ko'nsolga kiring")
+
+console.log( 'mening ismim  '+ ism) 
+console.log('sizning yoshingiz  '+ yosh);
+console.log('misol 17 + 9 = 26 sizning jovob '+ misol);
+console.log('misol  56 * 9 = 504 sizning jovob '+ misol1);
+console.log('misol 6 * 10 = 60 sizning jovob '+ misol2);
+console.log('misol 25 * 8 = 200 sizning jovob '+ misol3);
+console.log('misol 78 * 2 = 156 sizning jovob '+ misol4);
+console.log('misol  19 * 9 = 171 sizning jovob '+ misol5);
+console.log('misol  25 * 7 = 175 sizning jovob '+ misol6);
+console.log('misol 39 * 4 = 156 sizning jovob '+ misol7);
